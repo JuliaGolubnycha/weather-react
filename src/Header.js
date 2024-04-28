7bd356bb0b8b47151402688e789137141c57855f
YuliiaHolubnycha <61736018+JuliaGolubnycha@users.noreply.github.com>
2024-04-29 00:05:49 +0300

    Updated index.js and deleted App.js to fix deployment

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="body1">
+      <h1>Naboo Weather</h1>
+    </div>
+  );
+}
---
+++ src/weather.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  const [temperature, setTemperature] = useState(null);
+  const city_name = props.city;
+  const apiKey = "ed238469f9b5e9d801834270e65449bc";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city_name +
+    "&appid=" +
+    apiKey +
+    "&units=metric";
+
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setTemperature(response.data.main.temp);
+      })
+      .catch(error => {
+        console.error("Error fetching weather data:", error);
+      });
+  }, [url]);
+
+  return (
+    <h1>
+      {temperature}°C
+    </h1>
+  );
+}
